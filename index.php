991526990da80a31d7b5b86f1ada16ef65153a45
amhaualamar@orange.fr <Amar11>
2018-05-15 11:15:17 +0200

    php2

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE>
+<html>
+<title>Exercice3</title>
+<meta  charset="UTF-8">
+</head>
+<body>
+  <?php
+    $chiffre = 0;
+    while ($chiffre <= 10)
+  {echo $chiffre ++;}
+  ?>
+  <hr>
+  <?php
+    $a = 0;
+    $b = 15;
+    while ($a <=20)
+    {$c = $a * $b; echo $c; $a++;}
+  ?>
+  <hr>
+  <?php
+    $a = 100;
+    $b = 20;
+    while ($a >= 10)
+    {$c = $a * $b; echo $c; $a--;}
+   ?>
+</body>
+</html>
